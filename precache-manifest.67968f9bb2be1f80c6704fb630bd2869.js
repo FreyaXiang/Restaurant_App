989df2e7ea8685d16dfd3e554e05b06a695407d5
FreyaXiang <xiangxin0511@gmail.com>
2020-08-23 23:55:40 +0800

    Updates

---
+++ precache-manifest.67968f9bb2be1f80c6704fb630bd2869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab0a11966796bfc886d96f14f8dc737",
+    "url": "/Restaurant_App/index.html"
+  },
+  {
+    "revision": "842ff41859e6b994953e",
+    "url": "/Restaurant_App/static/js/2.9bd475ef.chunk.js"
+  },
+  {
+    "revision": "f01cf20f87878beb20bf2f939f8352f1",
+    "url": "/Restaurant_App/static/js/2.9bd475ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31212bebfe1bceed609",
+    "url": "/Restaurant_App/static/js/main.21b18563.chunk.js"
+  },
+  {
+    "revision": "69756c171a20331e24e5",
+    "url": "/Restaurant_App/static/js/runtime-main.7dc63524.js"
+  }
+]);
